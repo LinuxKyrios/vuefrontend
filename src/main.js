61bcefe6f24a65990979bfc73c94546758275d0f
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-19 20:25:20 +0200

    Installed font awesome and added to project with jquery and bootstrap.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min";
+import "bootstrap/dist/js/bootstrap.min";
+import "@fortawesome/vue-fontawesome/src/components/FontAwesomeIcon";
+import "@fortawesome/vue-fontawesome/src/components/FontAwesomeLayers";
+import "@fortawesome/vue-fontawesome/src/components/FontAwesomeLayersText";
 
 new Vue({
   render: h => h(App),
