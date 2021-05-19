aa832006e9bf38a3543733612ec97faa36e9af6c
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-19 23:02:20 +0200

    Created navigation and home section

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,41 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
-
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="#home" class="nav-link">Stona główna</a>
+          </li>
+          <li class="nav-item">
+            <a href="#distros" class="nav-link">Dystrybucje</a>
+          </li>
+          <li class="nav-item">
+            <a href="#features" class="nav-link">Możliwości</a>
+          </li>
+          <li class="nav-item">
+            <a href="#resources" class="nav-link">Zasoby</a>
+          </li>
+          <li class="nav-item">
+            <a href="#bootstrap" class="nav-link">Bootstrap</a>
+          </li>
+          <li class="nav-item">
+            <a href="#contact" class="nav-link">Kontakt</a>
+          </li>
+        </ul>
       </div>
-
     </nav>
+    <!--End navigation-->
+    <div class="landing">
+      <div class="come-wrap">
+        <div class="home-inner"></div>
+      </div>
+    </div>
+    <div class="text-center caption">
+      <h1>First Vue Project with enhanced frontend</h1>
+      <h3>Project with enhanced backend will be created in the close future</h3>
+      <a class="btn btn-outline-dark btn-lg" href="#resource">Load for more!</a>
+    </div>
   </div>
+<!-- End home section -->
 </template>
 
 <script>
